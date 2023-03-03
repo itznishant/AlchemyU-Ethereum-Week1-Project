f3a517effdebd178d9a65d1cb93b99ba9defa687
Nishant <nishant@Nishants-MacBook-Air.local>
2023-03-03 12:35:39 +0530

    cleanup files

--- server/scripts/generateSignature.js
+++ server/scripts/generateSignature.js
@@ -20,18 +20,10 @@ export async function generateSignedDigitalSignature(PRIVATE__KEY) {
 	        const ts = Date.now();
 
 	        const hashedMessage             = await keccak256(utf8ToBytes(message+ts));
-	        // const signature  = await secp.sign(hashedMessage, VALID_PRIVATE_KEY, {recovered: false} );
 	        const [signature, recoveryBit]  = await secp.sign(hashedMessage, VALID_PRIVATE_KEY, {recovered: true} );
 
 	        const PUBLIC_KEY_RECOVERED      = await secp.recoverPublicKey(hashedMessage, signature, recoveryBit);
-	        
-	        // console.log(toHex(keccak256(PUBLIC_KEY_RECOVERED).slice(-20)));
-	        // console.log([toHex(signature), recoveryBit] );
         }
     }
     return hashedMessage;
 }
-
-// generateSignedDigitalSignature("e5c018e22b1671a4386fa3de4f74a5993921da69b2bd4a9c78b707bf7066fa04");
-// generateSignedDigitalSignature("b09c78f7b539aa689af1c06e2ecc7b9a428bab0d6253105f3fbf990157e9aa0a");
-// generateSignedDigitalSignature("19337f19c16c7dfb731f5a92ae41bdee9e7304e1755a1baf5eae8525527403e0");
